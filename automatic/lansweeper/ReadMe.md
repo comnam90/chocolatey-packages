30cff67a8eb494161ef21767efdebb06a1b5041b
Ben Thomas (Admin) <a-bent@AltoStratus.IO>
2020-07-24 16:32:47 +1200

    Initial lansweeper commit

---
+++ automatic/lansweeper/ReadMe.md
@@ -0,0 +1,41 @@
+﻿# <img src="https://27jts3o00yy49vo2y30wem91-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/LsAgent-Scanning-Agent.png" width="48" height="48"/> [lsagent](https://chocolatey.org/packages/lansweeper)
+
+LsAgent is a small, lightweight application that you can install on your Windows, Mac and Linux devices. It gathers the asset data locally and then sends it back to your Lansweeper installation, either by using a direct push or through Lansweeper's cloud-hosted relay service.
+
+## Features
+
+* Automatically collect an inventory from a computer and sends the data back to a Lansweeper installation, either directly or through the lansweeper relay server in the cloud.
+* Does not require scanning credentials in Lansweeper.
+* Does not require administrative privileges to be able to scan.
+* Does not require configuration of your computers' firewalls for scanning. It does require an outbound connection to your Lansweeper installation or cloud relay server.
+
+## Package parameters
+
+The following parameters can be used:
+
+* `/upgrade`: to update a Lansweeper installation.
+* `/parts`: to specify which components to install (e.g. /parts ="SCAN,DB,WEB").
+* `/dbserver`: SQLLocalDB or SQLServer (e.g. /dbserver="SQLServer").
+* `/dbinstance`: to choose the SQL Server instance to connect to (e.g. /dbinstance="localhost\sqlexpress").
+* `/dbuser`: to specify the database username that creates the database during installation.
+* `/dbpassword`: to specify the database password of the user that creates the database during installation.
+* `/dbuserconfig`: optional password for the lansweeperuser database user, useful when installing multiple scanning servers.
+* `/allowdboverwrite`: forces the installer to overwrite an existing database found during a new Lansweeper installation.
+* `/webserver`: IIS or IISExpress.
+* `/httpport`: to specify the web console's HTTP port (e.g. httpport=81).
+* `/httpsport`: to specify the web console's HTTPS port (e.g. httpsport=82).
+* `/folder`: to specify the Lansweeper installation folder.
+* `/credkeyfile`: to specify the encryption key file (Encryption.txt) to be used.
+* `/noDCOMReset`: to prevent the Lansweeper installer from making DCOM changes on the Lansweeper server.
+* `/ConfigurationFile`: to perform the installation by pointing to a config file that contains the necessary parameters.
+
+Example: `choco install lansweeper --params "/dbserver=sqlserver /dbinstance=localhost\sqlexpress /allowdboverwrite"`
+
+All parameters and values are case-insensitive, except the values provided for /dbuserconfig and /dbpassword.  
+If a specific parameter is not provided, a default value is assumed. For instance, if you don't specify which components to install, the assumption is made that you want to install all of them.  
+If the installation stops unexpectedly, its log file can be found by typing %temp% in the path of Windows Explorer. The log file is a .txt and will start with "Setup Log...".  
+The final installation log file can be also found in Program Files (x86)\Lansweeper\Install on your Lansweeper server once the installation has fully completed.
+
+## Notes
+
+* `upgrade` parameter must be specified when upgrading an existing installation, otherwise it will overwrite the existing installation.
---
+++ automatic/lansweeper/tools/chocolateyinstall.ps1
@@ -0,0 +1,177 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# Helper Function
+
+function Test-InstallerParameters {
+  [cmdletbinding(DefaultParameterSetName="Default")]
+  param(
+    [parameter(ParameterSetName = "Upgrade", Mandatory)]
+    [switch]$Upgrade,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript(
+      {
+        if($_ -imatch '^(scan|db|web),*(scan|db|web)*,*(scan|db|web)*'){
+          $true
+        }else{
+          throw "/parts must contain at least one valid option (SCAN,DB,WEB)"
+        }
+      }
+    )]
+    [string]$Parts,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateSet("SqlLocalDb","SqlServer")]
+    [string]$DbServer,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateNotNullOrEmpty()]
+    [string]$DbInstance,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateNotNullOrEmpty()]
+    [string]$DbUser,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateNotNullOrEmpty()]
+    [string]$DbPassword,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateNotNullOrEmpty()]
+    [string]$DbUserConfig,
+    [parameter(ParameterSetName = "Default")]
+    [switch]$AllowDbOverwrite,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateSet("IISExpress","IIS")]
+    [string]$WebServer,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateNotNullOrEmpty()]
+    [uint32]$HttpPort,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateNotNullOrEmpty()]
+    [uint32]$HttpsPort,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateNotNullOrEmpty()]
+    [string]$Folder,
+    [parameter(ParameterSetName = "Default")]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript(
+      {
+        if( (Test-Path -Path $_) ){
+          $true
+        }else{
+          throw "Cannot find encryption file $_"
+        }
+      }
+    )]
+    [string]$CredKeyFile,
+    [parameter(ParameterSetName = "Default")]
+    [parameter(ParameterSetName = "ConfigurationFile")]
+    [switch]$NoDcomReset,
+    [parameter(ParameterSetName = "ConfigurationFile", Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript(
+      {
+        if( (Test-Path -Path $_) ){
+          $true
+        }else{
+          throw "Cannot find configuration file $_"
+        }
+      }
+    )]
+    [string]$ConfigurationFile
+  )
+  begin{
+    if($PSCmdlet.ParameterSetName -ieq "Default"){
+      if($PSBoundParameters.containsKey('DbInstance') -and -not $PSBoundParameters.containsKey('DbServer')){
+        Throw "/dbserver must be included when using /dbinstance"
+      }
+      if($PSBoundParameters.containsKey('DbUser') -and -not $PSBoundParameters.containsKey('DbServer')){
+        Throw "/dbserver must be included when using /dbuser"
+      }
+      if($PSBoundParameters.containsKey('DbPassword') -and -not $PSBoundParameters.containsKey('DbServer')){
+        Throw "/dbserver must be included when using /dbpassword"
+      }
+      if($PSBoundParameters.containsKey('DbUser') -and -not $PSBoundParameters.containsKey('DbPassword')){
+        throw "/dbuser must be included when using /dbpassword"
+      }
+      if($PSBoundParameters.containsKey('DbPassword') -and -not $PSBoundParameters.containsKey('DbUser')){
+        throw "/dbpassword must be included when using /dbuser"
+      }
+      if($PSBoundParameters.containsKey('DbUserConfig') -and -not $PSBoundParameters.containsKey('DbServer')){
+        Throw "/dbserver must be included when using /dbuserconfig"
+      }
+      if($PSBoundParameters.containsKey('HttpPort') -and -not $PSBoundParameters.containsKey('WebServer')){
+        Throw "/webserver must be included when using /httpport"
+      }
+      if($PSBoundParameters.containsKey('HttpsPort') -and -not $PSBoundParameters.containsKey('WebServer')){
+        Throw "/webserver must be included when using /httpsport"
+      }
+    }
+  }
+  process{
+    $returnParameters = switch($PSBoundParameters.Keys){
+      "parts" { "/parts=""$Parts""" }
+      "dbserver" { "/dbserver=""$DbServer""" }
+      "dbinstance" { "/dbinstance=""$DbInstance""" }
+      "dbuser" { "/dbuser=""$DbUser""" }
+      "dbpassword" { "/dbpassword=""$DbPassword""" }
+      "dbuserconfig" { "/dbuserconfig=""$dbuserconfig""" }
+      "allowdboverwrite" { "/allowdboverwrite" }
+      "webserver" { "/webserver=""$webserver""" }
+      "httpport" { "/httpport=$httpport" }
+      "httpsport" { "/httpsport=httpsport" }
+      "folder" { "/folder=""$folder""" }
+      "credkeyfile" { "/credkeyfile=""$credkeyfile""" }
+      "noDCOMReset" { "/noDCOMReset" }
+      "ConfigurationFile" { "/ConfigurationFile=""$ConfigurationFile""" }
+    }
+  }
+  end{
+    $returnParameters -join " "
+  }
+}
+
+
+$PP = Get-PackageParameters
+$InstallParams = " "
+$InstallParams += Test-InstallerParameters @PP -Verbose
+Write-Host "Installer Parameters = $InstallParams"
+<#
+if($PP['parts']){$InstallParams += "/parts=""$($PP['parts'])"" "}
+if($PP['dbserver']){$InstallParams += "/dbserver=""$($PP['dbserver'])"" "}
+if($PP['dbinstance']){$InstallParams += "/dbinstance=""$($PP['dbinstance'])"" "}
+if($PP['dbuser']){$InstallParams += "/dbuser=""$($PP['dbuser'])"" "}
+if($PP['dbpassword']){$InstallParams += "/dbpassword=""$($PP['dbpassword'])"" "}
+if($PP['dbuserconfig']){$InstallParams += "/dbuserconfig=""$($PP['dbuserconfig'])"" "}
+if($PP['allowdboverwrite']){$InstallParams += "/allowdboverwrite=""$($PP['allowdboverwrite'])"" "}
+if($PP['webserver']){$InstallParams += "/webserver=""$($PP['webserver'])"" "}
+if($PP['httpport']){$InstallParams += "/httpport=$($PP['httpport']) "}
+if($PP['httpsport']){$InstallParams += "/httpsport=$($PP['httpsport']) "}
+if($PP['folder']){$InstallParams += "/folder=""$($PP['folder'])"" "}
+if($PP['credkeyfile']){$InstallParams += "/credkeyfile=""$($PP['credkeyfile'])"" "}
+if($PP['noDCOMReset']){$InstallParams += "/noDCOMReset=""$($PP['noDCOMReset'])"" "}
+if($PP['ConfigurationFile']){$InstallParams += "/ConfigurationFile=""$($PP['ConfigurationFile'])"" "}
+
+if( ($PP['httpport'] -or $PP['httpsport']) -and ! $PP['webserver'] ){
+  Write-Error -Message "/httpport or /httpsport cannot be specified without also providing /webserver"
+}
+#>
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = 'https://cdn.lansweeper.com/download/8.0.130/23/LansweeperSetup.exe'
+  url64bit      = 'https://cdn.lansweeper.com/download/8.0.130/23/LansweeperSetup.exe'
+  softwareName  = 'Lansweeper'
+  checksum      = 'fac81e47b7f6c916c6624638fef87a6522b89322e9e06955119ad05cd10ee2b9'
+  checksumType  = 'sha256'
+  checksum64    = 'fac81e47b7f6c916c6624638fef87a6522b89322e9e06955119ad05cd10ee2b9'
+  checksumType64= 'sha256'
+  silentArgs   = '/verysilent /accepteula /install',$InstallParams
+  validExitCodes= @(0)
+}
+
+if($PP['Upgrade']){
+  $packageArgs.silentArgs = $packageArgs.silentArgs.Replace("/install","/upgrade")
+}
+
+Install-ChocolateyPackage @packageArgs 
